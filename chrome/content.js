0a8ddee82d0a914d477960974bc17a5992a27b82
claire guo <claireguo@macbook-pro-126.risd.edu>
2023-11-08 13:20:26 -0500

    v2

--- chrome/content.js
+++ chrome/content.js
@@ -1,66 +1,127 @@
+//REPLACE Cs WITH LINKS + ROTATE IMGS
 function replace() {
-//   const textNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-
-// while (textNodes.nextNode()) {
-//   const textNode = textNodes.currentNode;
-//   const text = textNode.nodeValue;
-//   const newText = text.replace(/c/g, '<span style="background-color: yellow;">c</span>');
-//   const newTextNode = document.createElement('span');
-//   newTextNode.innerHTML = newText;
-//   textNode.parentNode.replaceChild(newTextNode, textNode);
-// }
-const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
-// for (let i=0; i < text.length; i++) {
-//   console.log(text[i].innerHTML.includes("c"))
-//   if (text[i].innerHTML.includes('c')) {
-//     text[i].innerHTML.replace('c', 'ooooooooooooo')
-//   }
-// }
-function replaceTextWithLink(text) {
-  return text.replace(/c/g, '<a href="https://claireguo.com" target="_blank">c</a>');
+  function replaceTextWithLink(text) {
+    return text.replace(/c/g, '<a class="claire" href="https://claireguo.com" target="_blank">c</a>');
+  }
+  const elements = document.getElementsByTagName('p', 'h1', 'span', 'h2', 'h3', 'h4', 'li', 'td', 'a');
+  for (const element of elements) {
+      const text = element.textContent;
+      element.innerHTML = replaceTextWithLink(text)
+  }
+  const imgs = document.getElementsByTagName('img');
+  for (el of imgs) {
+    el.style['transform'] = 'rotate(180deg)'
+  }
 }
-
-const elements = document.getElementsByTagName('p', 'h1', 'span', 'h2', 'h3', 'h4', 'li', 'td', 'a');
-for (const element of elements) {
-  console.log(element.textContent)
-    const text = element.textContent;
-    const newText = text.replace(/o/g, 'ooooo');
-    if (newText !== text) {
-      element.textContent = newText;
-      element.innerHTML = replaceTextWithLink(newText)
-    }
-}
-
-const imgs = document.getElementsByTagName('img');
-for (el of imgs) {
-  el.style['transform'] = 'rotate(180deg)'
-}
-
-let paragraphs = document.getElementsByTagName('p', 'h1', 'span', 'h2');
-for (elt of paragraphs) {
-  elt.style['background-color'] = '#00b85c'
-}
-}
-
 replace();
 
-function myFunction(event) {
-      const emoji = document.getElementById('emoji');
 
-  if (event.key === 'm') {
-    alert('Key pressed: m');
-
-    // const maxX = window.innerWidth;
-    // const maxY = window.innerHeight;
-
-    // const randomX = Math.floor(Math.random() * maxX);
-    // const randomY = Math.floor(Math.random() * maxY);
-
-    // emoji.style['left'] = randomX + 'px';
-    // emoji.style['top'] = randomY + 'px';
-    // emoji.style['display'] = 'block';
-    //manipulating the DOM search
+//ALT TEXT INPUT
+const altContainer = document.createElement("div");
+altContainer.style.display = 'flex';
+altContainer.style.flexWrap = 'wrap';
+const imgs = document.getElementsByTagName('img');
+for (img of imgs) {
+  const alt = img.getAttribute('alt');
+  const altInput = document.createElement("input");
+  altInput.type = "text";
+  altInput.name = "alt text";
+  altInput.id = "altInput";
+  altInput.value = alt;
+  // img.parentNode.replaceChild(altInput, img);
+  altContainer.appendChild(altInput);
+  altInput.addEventListener('focus', () => {
+    altInput.style.backgroundColor = "#d7f542";
+  });
+  altInput.addEventListener("blur", () => {
+    altInput.style.backgroundColor = "";
+  });
 }
+document.body.appendChild(altContainer);
+
+
+
+//HIGHLIGHT C LINKS ON HOVER + STYLING
+const cLinks = document.getElementsByClassName("claire");
+for (el of cLinks) {
+  el.style['text-decoration'] = 'none';
+  el.style['color'] = 'blue';
+  el.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = '#d7f542';
+  });
+  el.addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor = '';
+  });
 }
 
-document.addEventListener('keydown', myFunction);
+
+//COLOR PICKER
+const container = document.createElement("div");
+const sparkle = document.createElement("span");
+const label = document.createElement("label");
+const colorPicker = document.createElement("input");
+
+container.style.position = "absolute";
+container.style.top = "100px";
+container.style.left = "100px";
+container.style.backgroundColor = "white";
+container.style.borderRadius = "1.5rem";
+container.style.padding = "0.5rem 1.25rem"
+sparkle.textContent = "⋆⭒˚｡⋆₊ ⊹ good job ⋆⭒˚｡⋆₊ ⊹";
+sparkle.style.backgroundColor = "#d7f542";
+label.textContent = "pick a color ";
+label.setAttribute("for", "colorPicker");
+colorPicker.type = "color";
+container.appendChild(label);
+container.appendChild(colorPicker);
+
+colorPicker.addEventListener("input", () => {
+  container.appendChild(sparkle);
+
+  setTimeout(() => {
+    // sparkle.style.opacity = "0";
+    container.removeChild(sparkle);
+  }, 500);
+})
+document.body.appendChild(container);
+
+
+//CLICK AND DRAG HEADINGS
+const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+console.log(headings);
+let mousePosition;
+let offset = [0,0];
+let isDown = false;
+let selected = null;
+
+headings.forEach(elt => {
+  elt.style['background-color'] = '#d7f542';
+  elt.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+      elt.offsetLeft - e.clientX,
+      elt.offsetTop - e.clientY
+    ];
+    selected = elt;
+  }, true);
+});
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+    selected = null;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown && selected) {
+        mousePosition = {
+
+            x : event.clientX,
+            y : event.clientY
+
+        };
+        selected.style.position = "absolute";
+        selected.style.left = (mousePosition.x + offset[0]) + 'px';
+        selected.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);
